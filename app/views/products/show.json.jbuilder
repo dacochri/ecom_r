2cc6852e627f1943cd1dc596734a92777d04e3a2
dacochri <iramortis@gmail.com>
2014-07-02 19:42:47 -0400

    product and category scaffold

---
+++ app/views/products/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @product, :id, :category_id, :name, :image, :price, :manufacturer, :quantity, :description, :created_at, :updated_at
---
+++ db/migrate/20140702233315_create_products.rb
@@ -0,0 +1,15 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.integer :category_id
+      t.string :name
+      t.string :image
+      t.decimal :price
+      t.string :manufacturer
+      t.integer :quantity
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
