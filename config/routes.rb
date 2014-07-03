fac3fa11968018cf0698261ef921e1b63e42ffe5
dacochri <iramortis@gmail.com>
2014-07-02 20:08:33 -0400

    default route, change about & contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :products
-
   resources :categories
+  
+  get 'about' => 'index#about', :as => :about
+  get 'contact' => 'index#contact', :as => :contact
+
+  root :to => 'index#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
