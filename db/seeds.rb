87c5fe1da75189c654cd8d5e457139c838ccc3a7
dacochri <iramortis@gmail.com>
2014-07-08 13:39:49 -0400

    update seed with image

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 Category.create(category_id: nil, name: 'Electronics')
 Category.create(category_id: 1, name: 'Cellphones')
 
-Products.create(category_id: 1, name: 'Galaxy S2', image: nil, price: '599.99', manufacturer: 'Samsung', quantity: 3, description: nil)
+Products.create(category_id: 1, name: 'Galaxy S2', image: 'galaxys2.png', price: '599.99', manufacturer: 'Samsung', quantity: 3, description: 'Smartphone')
